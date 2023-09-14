a2b985de26c79cbd754016031dd033be840a872b
sebastianlopezs <135356217+sebastianlopezs@users.noreply.github.com>
2023-09-14 21:49:03 +0000

    arreglado x3

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
-const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const valores = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
 const valorAleatorio = Math.floor(Math.random() * valores.length);
 
 const carta = document.querySelector(".carta");
 
-carta.innerHTML = valores [valorAleatorio];
+carta.innerHTML = valores[valorAleatorio];
 
 const pintas = ["diamante", "corazon", "pica", "trebol"];
 const pintaAleatoria = Math.floor(Math.random() * pintas.length);
